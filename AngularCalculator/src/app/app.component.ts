e8f1eb9fecd8bb5942dffe3e220f6a680d6082e3
Leonidas Bozatzidis <154006329+BozatzidisLeonidas@users.noreply.github.com>
2024-06-12 23:26:40 +0300

    progress with app.html

--- AngularCalculator/src/app/app.component.ts
+++ AngularCalculator/src/app/app.component.ts
@@ -10,4 +10,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'AngularCalculator';
+
+  calValue: number = 0;
+  funcT: any = 'NoFunction';
+
+  calNumber: string = 'anyValue';
+
+  onClickValue (value : string, type : any) {
+    if(type == 'number'){
+      this.onNumberClick(value);
+    }
+  }
+
+  onNumberClick (value : string) {
+     if(this.calNumber != 'noValue'){
+      this.calNumber += value;
+     }
+  }
 }
